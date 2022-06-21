ad0f9748e2be7cabad24b1588c03ca6ce44b3d13
Bazylevich <victoria.bazylevich@gmail.com>
2022-06-21 21:40:52 +0300

    Add bubble sorter class

---
+++ src/by/itstep/bazylevich/javalessons/lesson18/controller/Task5.java
@@ -0,0 +1,34 @@
+package by.itstep.bazylevich.javalessons.lesson18.controller;
+import by.itstep.bazylevich.javalessons.lesson18.model.logic.ArraySorter;
+import by.itstep.bazylevich.javalessons.lesson18.model.logic.ArrayWorker;
+import by.itstep.bazylevich.javalessons.lesson18.util.ArrayInitializer;
+import by.itstep.bazylevich.javalessons.lesson18.util.Convertor;
+import by.itstep.bazylevich.javalessons.lesson18.view.Printer;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+
+        do {
+            Printer.print("Input size of array: ");
+            size = scanner.nextInt();
+        } while(size <= 0);
+
+        int[] array = new int [size];
+
+        //ArrayInitializer.userInit(array);
+        ArrayInitializer.randomInit(array);
+
+        Printer.print("Array before sort: " + Convertor.toString(array));
+
+        ArraySorter.bubbleSorAcs(array);
+        Printer.print("\nArray after sort: " + Convertor.toString(array));
+
+        ArrayWorker.reverse(array);
+        Printer.print("\nArray after rev: " + Convertor.toString(array));
+
+    }
+}
--- src/by/itstep/bazylevich/javalessons/lesson18/model/logic/ArraySorter.java
+++ src/by/itstep/bazylevich/javalessons/lesson18/model/logic/ArraySorter.java
@@ -2,7 +2,15 @@ package by.itstep.bazylevich.javalessons.lesson18.model.logic;
 
 public class ArraySorter {
     public static void bubbleSorAcs(int[] array) {
-
+        for (int j = 0; j < array.length-1; j++) {
+            for (int i = 0; i < array.length-1; i++) {
+                if (array[i]<array[i+1]){
+                    int temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                }
+            }
+        }
     }
     public static void selectedSorAcs(int[] array) {
 
